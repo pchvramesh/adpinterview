ea98511169f4eb28b9dd77a5c21816c8e229b5d5
Ramesh Kumar <8065767+pchvramesh@users.noreply.github.com>
2024-12-10 19:42:36 +0530

    Code optimization

--- server.js
+++ server.js
@@ -1,55 +1,54 @@
 const getTasksURL = 'https://interview.adpeai.com/api/v2/get-task'
 const submitTaskURL = 'https://interview.adpeai.com/api/v2/submit-task'
 
-//API services
-const getTasks = async () => {
-    return fetch(getTasksURL).then((resp) => resp.json())
-}
 
+//API services
+const makeAPICall = async (url, options = {}) => {
+    console.log(url, options)
+    return fetch(url, options);
+};
+
+
+//Get Tasks
+const getTasks = () => makeAPICall(getTasksURL).then((resp) => resp.json())
+
+//Submit Task
 const submitTask = async (requestObj) => {
-    console.log("Request Object : ",requestObj)
-    return fetch(submitTaskURL, {
+    return makeAPICall(submitTaskURL, {
         method: 'POST',
-        data: JSON.stringify(requestObj)
+        body: JSON.stringify(requestObj),
+        headers: {
+            'content-type': 'application/json'
+        }
     })
 }
 
 //Util Functions
-const filterTransactionsByYear = (data) => {
-    let cYear = new Date().getFullYear()
-    return data.filter((e) => {
-        const year = e.timeStamp.split('-')[0]
-        return cYear - 1 === Number(year)
-    })
+const filterTransactionsByYear = (data, year) => {
+    return data.filter(({ timeStamp }) => timeStamp.startsWith(`${year}`))
 }
 
 const groupByEmployee = (filtered) => {
     const data = {}
-    filtered.forEach(e => {
-        if (!data[e.employee.id]) {
-            data[e.employee.id] = {
-                transactions: [],
+    filtered.forEach(({ employee: { id }, amount, transactionID, type }) => {
+        if (!data[id]) {
+            data[id] = {
                 sum: 0,
                 ids: []
             }
         }
-        data[e.employee.id].transactions.push(e)
+        data[id].sum += amount
+
+        if (type === 'alpha') {
+            data[id].ids.push(transactionID)
+        }
     });
-
-    Object.keys(data).forEach((key) => {
-        data[key].transactions.forEach((e) => {
-            data[key].sum += e.amount
-        })
-
-        // Get the transaction ids
-        data[key].ids = data[key].transactions.filter((e) => e.type === 'alpha').map((e) => e.transactionID)
-    })
 
     return Object.values(data)
 }
 
 const getHighestSumTotal = (data) => {
-    return data.reduce((max, obj) => obj.sum > max.sum ? obj : max)
+    return data.reduce((max, obj) => (obj.sum > max.sum ? obj : max), data[0])
 }
 
 
@@ -57,7 +56,8 @@ const getHighestSumTotal = (data) => {
 const fetchData = async () => {
     try {
         const data = await getTasks()
-        let filtered = filterTransactionsByYear(data.transactions)
+        const prevYear = new Date().getFullYear() - 1
+        let filtered = filterTransactionsByYear(data.transactions, prevYear)
         filtered = groupByEmployee(filtered)
         const highest = getHighestSumTotal(filtered)
         const requestObj = {
@@ -65,10 +65,10 @@ const fetchData = async () => {
             result: highest.ids
         }
         const resp = await submitTask(requestObj)
-        console.log("Response ",resp)
+        console.log("Response ", resp)
     }
     catch (e) {
-        console.log("ERROR : ",e)
+        console.log("ERROR : ", e)
     }
 }
 
